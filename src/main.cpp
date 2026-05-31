0cf42dd35bbc1409a19fa11e5b81b47b9aeefc00
unknown <bagtooheavy@gmail.com>
2026-05-31 20:13:16 +0530

    added view credentials feature

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,61 @@
 #include <iostream>
 #include <string>
-#include<vector>
-#include<fstream>
+#include <vector>
+#include <fstream>
+#include <sstream>
 using namespace std;
 
-class Credentials{
-    public:
-        string website;
-        string username;
-        string password;
+class Credentials
+{
+public:
+    string website;
+    string username;
+    string password;
 };
 
-vector <Credentials> cred;
+vector<Credentials> cred;
 void addCredentials();
+void loadCredentials();
+void viewCredentials();
 
-void menu(){
-    cout << "Password Manager" << endl << endl;
-    cout <<"1. Add Credential  "<<endl;
-    cout <<"2. View Credentials"<<endl;
-    cout<<"3. Exit"<<endl;
+void menu()
+{
+    cout << endl;
+    cout << "Password Manager" << endl
+         << endl;
+    cout << "1. Add Credential  " << endl;
+    cout << "2. View Credentials" << endl;
+    cout << "3. Exit" << endl;
     int x;
-    cin>>x ;
-    switch (x){
-        case 1:
+    cin >> x;
+    switch (x)
+    {
+    case 1:
         addCredentials();
         break;
-        /*case 2:
+
+    case 2:
         viewCredentials();
-        break;*/
-        case 3:
-        cout<<"Exiting program"<<endl;
-        return;
-        default:
-        cout<<"Enter valid credential"<<endl;
+        break;
+    case 3:
+        cout << "Exiting program" << endl;
+        exit(0);
+    default:
+        cout << "Enter valid credential" << endl;
     }
 }
-void addCredentials(){
+void addCredentials()
+{
     Credentials c;
-    cout<<"Enter website:"<<endl;
-    cin>>c.website;
-    cout<<"Enter username:"<<endl;
-    cin>>c.username;
-    cout<<"Enter password:"<<endl;
-    cin>>c.password;
+    cout << "Enter website:" << endl;
+    cin >> c.website;
+    cout << "Enter username:" << endl;
+    cin >> c.username;
+    cout << "Enter password:" << endl;
+    cin >> c.password;
     cred.push_back(c);
-    ofstream file("passwords.txt", ios::app);
-    if(!file)
+    ofstream file("../data/passwords.txt", ios::app);
+    if (!file)
     {
         cout << "Error opening file\n";
         return;
@@ -57,13 +67,56 @@ void addCredentials(){
          << c.password
          << endl;
     file.close();
-    cout<<"Credentials added succesfully!"<<endl;
+    cout << "Credentials added succesfully!" << endl;
+}
+void loadCredentials()
+{
+    ifstream file("../data/passwords.txt");
+    if (!file)
+    {
+        cout << "No password file found.\n";
+        return;
+    }
+    string line;
+    while (getline(file, line))
+    {
+        stringstream ss(line);
+        string website;
+        string username;
+        string password;
+        getline(ss, website, '|');
+        getline(ss, username, '|');
+        getline(ss, password, '|');
+        Credentials c;
+        c.website = website;
+        c.password = password;
+        c.username = username;
+        cred.push_back(c);
+    }
+    file.close();
+}
+void viewCredentials()
+{
+    if (cred.empty())
+    {
+        cout << "No credentials stored.\n";
+        return;
+    }
 
+    for (auto &c : cred)
+    {
+        cout << "\nWebsite: " << c.website;
+        cout << "\nUsername: " << c.username;
+        cout << "\nPassword: " << c.password;
+        cout << "\n---------------------\n";
+    }
 }
 int main()
 {
-    menu();
+    loadCredentials();
+    while (true)
+    {
+        menu();
+    }
     return 0;
-
-
 }
