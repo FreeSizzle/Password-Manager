a73df70fb289ac05f753882134663677952eebee
unknown <bagtooheavy@gmail.com>
2026-06-02 21:01:24 +0530

    added search and deletion features

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,8 @@ vector<Credentials> cred;
 void addCredentials();
 void loadCredentials();
 void viewCredentials();
-
+void searchCredential();
+void deleteCredential();
 void menu()
 {
     cout << endl
@@ -25,9 +26,12 @@ void menu()
          << endl
          << "1. Add Credential  " << endl
          << "2. View Credentials" << endl
-         << "3. Exit" << endl;
+         << "3. Search Credential" << endl
+         << "4. Delete Credential"<<endl
+         << "5. Exit"<<endl;
     int x;
     cin >> x;
+    cin.ignore();
     switch (x)
     {
     case 1:
@@ -38,6 +42,12 @@ void menu()
         viewCredentials();
         break;
     case 3:
+        searchCredential();
+        break;
+    case 4:
+        deleteCredential();
+        break;
+    case 5:
         cout << "Exiting program" << endl;
         exit(0);
     default:
@@ -71,6 +81,7 @@ void addCredentials()
 }
 void loadCredentials()
 {
+    cred.clear();
     ifstream file("../data/passwords.txt");
     if (!file)
     {
@@ -89,8 +100,9 @@ void loadCredentials()
         getline(ss, password, '|');
         Credentials c;
         c.website = website;
-        c.password = password;
         c.username = username;
+        c.password = password;
+
         cred.push_back(c);
     }
     file.close();
@@ -110,6 +122,49 @@ void viewCredentials()
         cout << "\nPassword: " << c.password;
         cout << "\n---------------------\n";
     }
+}
+void searchCredential(){
+    cout<<"Enter username to search :"<<endl;
+    string target;
+    cin>>target;
+    int flag =0;
+    for (auto x :cred){
+        if(x.username  == target){
+            cout<<"Credential Found"<<endl;
+            flag =1;
+            cout<<x.website<<"|"<<x.username<<"|"<<x.password<<endl;
+        }
+    }
+    if(flag ==0) cout<<"Credential Not Found"<<endl;
+}
+void deleteCredential(){
+    cout<<"Enter username to delete:"<<endl;
+    string target;
+    cin>>target;
+    auto it = cred.begin();
+    int flag =0;
+    while (it != cred.end()) {
+        if (it->username == target) {
+             it = cred.erase(it);
+             cout<<"Credential Deleted"<<endl;
+             flag = 1;
+        }
+        else {
+             ++it;
+        }
+    }
+    if(flag ==0)cout<<"Credential Not Found"<<endl;
+
+    ofstream file("../data/passwords.txt", ios::out);
+    for(auto c :cred){
+        file << c.website
+         << "|"
+         << c.username
+         << "|"
+         << c.password
+         << endl;
+    }
+    file.close();
 }
 int main()
 {
